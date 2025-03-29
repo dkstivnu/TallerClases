79d5b397cf4f5ab5e184e3e065b4695908a33f16
stivn <wilstivenortega@gmail.com>
2025-03-29 00:03:52 -0500

    add validaciones estudiante

--- src/LogicaPresentacion/MainEstudiante.java
+++ src/LogicaPresentacion/MainEstudiante.java
@@ -23,27 +23,42 @@ public class MainEstudiante {
             switch (opcion) {
                 case 1: {
                     System.out.print("Ingrese la primera nota: ");
-                    estudiante.setNotaExam1(sc.nextDouble());
+                    estudiante.setNotaExam1(obtenerNota(sc));
 
                     System.out.print("Ingrese la segunda nota: ");
-                    estudiante.setNotaExam2(sc.nextDouble());
+                    estudiante.setNotaExam2(obtenerNota(sc));
 
                     System.out.print("Ingrese la terca nota: ");
-                    estudiante.setNotaExam3(sc.nextDouble());
+                    estudiante.setNotaExam3(obtenerNota(sc));
                 }
                 break;
                 case 2:
-                    System.out.print(estudiante.getInformacion());
+                    System.out.println(estudiante.getInformacion());
                     break;
                 case 3:
                     System.out.println("La nota final del estudiante es: "+ estudiante.calcNotaFinal());
                     break;
                 case 4:
                     break;
+                    default:
+                        mensajeError(opcion);
             }
         } while (opcion != 4);
 
 
+    }
+
+    private static double obtenerNota(Scanner sc) {
+        double nota;
+        do {
+            nota = sc.nextDouble();
+
+            if (nota < 0) {
+                System.out.println("La nota no puede ser negativa, ingresa nuevamente: ");
+            }
+
+        }while (nota < 0);
+        return nota;
     }
 
     private static void imprimirMenu() {
@@ -53,5 +68,13 @@ public class MainEstudiante {
         System.out.println("3. Calcular nota final");
         System.out.println("4. Salir");
 
+    }
+
+    private static void mensajeError(int opcion) {
+        if (opcion < 0) {
+            System.out.println("La opcion no es valida! Debe ser positiva");
+        } else {
+            System.out.println("La opcion no es valida! Debe ser diferente de cero o menor que 4");
+        }
     }
 }
